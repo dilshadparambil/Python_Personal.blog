1cd3c258ad4dabfa8dd1d4b1af252967023d352b
Dilshad P <dilshadkareemparambil@gmail.com>
2025-10-09 10:01:43 +0530

    updated requirements

--- d70.py
+++ d70.py
@@ -3,7 +3,7 @@
 import os
 import smtplib
 from datetime import date
-from flask import Flask, abort, render_template, redirect, url_for, flash, request
+from flask import Flask, abort, render_template, redirect, url_for, flash
 from flask_bootstrap import Bootstrap5
 from flask_ckeditor import CKEditor
 from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
